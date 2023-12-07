ce8dc1fee491197f674d3aefb4d26d00aa8efc65
Danya1104 <151004452+Danya1104@users.noreply.github.com>
2023-12-07 18:39:47 +0300

    Update Set.cpp

--- Project10/Set.cpp
+++ Project10/Set.cpp
@@ -16,61 +16,20 @@ int Set::GetMaxPower() const {
     return MaxPower;
 }
 
-//int Set::IsMember(const int Elem) const {
-//    if (Elem < 0 || Elem >= MaxPower) throw std::out_of_range("No correct Elem");
-//    return BitField.GetBit(Elem);
-//}
-//
-//void Set::InsElem(const int Elem) {
-//    if (Elem < 0 || Elem >= MaxPower) throw std::out_of_range("No correct Elem");
-//    BitField.SetBit(Elem);
-//}
-//
-//void Set::DelElem(const int Elem) {
-//    if (Elem < 0 || Elem >= MaxPower) throw std::out_of_range("No correct Elem");
-//    BitField.ClrBit(Elem);
-//}
- int Set::IsMember(const int Elem) const {
-if (Elem >= 0 && Elem < MaxPower) {
+int Set::IsMember(const int Elem) const {
+    if (Elem < 0 || Elem >= MaxPower) throw std::out_of_range("No correct Elem");
     return BitField.GetBit(Elem);
-}
-throw std::out_of_range("No correct Elem");
 }
 
 void Set::InsElem(const int Elem) {
-    if (Elem >= 0 && Elem < MaxPower) {
-        BitField.SetBit(Elem);
-    }
-    else {
-        throw std::out_of_range("No correct Elem");
-    }
+    if (Elem < 0 || Elem >= MaxPower) throw std::out_of_range("No correct Elem");
+    BitField.SetBit(Elem);
 }
 
 void Set::DelElem(const int Elem) {
-    if (Elem >= 0 && Elem < MaxPower) {
-        BitField.ClrBit(Elem);
-    }
-    else {
-        throw std::out_of_range("No correct Elem");
-    }
+    if (Elem < 0 || Elem >= MaxPower) throw std::out_of_range("No correct Elem");
+    BitField.ClrBit(Elem);
 }
-
-//int Set::IsMember(const int Elem) const {
-//    if (Elem < 0 || Elem >= MaxPower) {
-//        std::cerr << "Error: No correct Elem. Elem = " << Elem << std::endl;
-//        throw std::out_of_range("No correct Elem");
-//    }
-//    return BitField.GetBit(Elem);
-//}
-//
-//void Set::InsElem(const int Elem) {
-//    BitField.SetBit(Elem);
-//}
-//
-//void Set::DelElem(const int Elem) {
-//    BitField.ClrBit(Elem);
-//}
-
 
 Set& Set::operator=(const Set& s) {
     if (this != &s) {
@@ -115,79 +74,25 @@ Set Set::operator~() const {
     return tmp;
 }
 
-//std::istream& operator>>(std::istream& istr, Set& s) {
-//    int Elem;
-//    for (int i = 0; i < s.MaxPower; i++) {
-//        istr >> Elem;
-//        if (Elem >= 0 && Elem < s.MaxPower) {
-//            s.InsElem(Elem);
-//        }
-//        else {
-//            throw std::out_of_range("No correct Elem");
-//        }
-//    }
-//    return istr;
-//}
-
-//std::istream& operator>>(std::istream& istr, Set& s) {
-//    int Elem;
-//    for (int i = 0; i < s.MaxPower; i++) {
-//        istr >> Elem;
-//        if (Elem >= 0 && Elem < s.MaxPower) {
-//            s.InsElem(Elem);
-//        }
-//        else {
-//            std::cerr << "Error: No correct Elem. Elem = " << Elem << std::endl;
-//            throw std::out_of_range("No correct Elem");
-//        }
-//    }
-//    return istr;
-//}
-//std::istream& operator>>(std::istream& istr, Set& s) {
-//    int Elem;
-//    std::cout << "Введите элементы множества через пробел (размерность " << s.MaxPower << "): ";
-//    for (int i = 0; i < s.MaxPower; ++i) {
-//        istr >> Elem;
-//        if (Elem >= 0) {
-//            s.InsElem(Elem);
-//        }
-//        else {
-//            std::cerr << "Error: No correct Elem. Elem = " << Elem << std::endl;
-//            throw std::out_of_range("No correct Elem");
-//        }
-//    }
-//    return istr;
-//}
-//std::istream& operator>>(std::istream& istr, Set& s) {
-//    int Elem;
-//    std::cout << "Введите элементы множества через пробел (размерность " << s.MaxPower << "): ";
-//
-//    while (istr >> Elem) {
-//        s.InsElem(Elem);
-//    }
-//
-//    return istr;
-//}
 std::istream& operator>>(std::istream& istr, Set& s) {
-    int ElemCount = 0;  // Переменная для подсчета введенных элементов
     int Elem;
-
-    std::cout << "Введите элементы множества через пробел (размерность " << s.MaxPower << "): ";
-
-    while (ElemCount < s.MaxPower && istr >> Elem) {
-        s.InsElem(Elem);
-        ElemCount++;
+    for (int i = 0; i < s.MaxPower; i++) {
+        istr >> Elem;
+        if (Elem >= 0 && Elem < s.MaxPower) {
+            s.InsElem(Elem);
+        }
+        else {
+            throw std::out_of_range("No correct Elem");
+        }
     }
-
     return istr;
 }
-
 
 std::ostream& operator<<(std::ostream& ostr, const Set& s) {
     ostr << std::endl << "{";
     for (int i = 0; i < s.MaxPower; i++) {
         if (s.IsMember(i)) {
-            ostr << i << ", "; // выводите реальные элементы множества, а не индексы битов
+            ostr << i + 1 << ", "; // РІС‹РІРѕРґРёС‚Рµ СЂРµР°Р»СЊРЅС‹Рµ СЌР»РµРјРµРЅС‚С‹ РјРЅРѕР¶РµСЃС‚РІР°, Р° РЅРµ РёРЅРґРµРєСЃС‹ Р±РёС‚РѕРІ
         }
     }
 
