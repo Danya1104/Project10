6f12086798fb84afb8db0e90a1d13918d284316a
Danya1104 <151004452+Danya1104@users.noreply.github.com>
2023-12-19 15:42:38 +0300

    Update Set.h

--- Project10/Set.h
+++ Project10/Set.h
@@ -1,33 +1,31 @@
-#pragma once
-
 #include "Bitefield.h"
 
 class Set {
-public:
-    Set(int mp);
-    Set(const Set& s);
-    Set(const Bitefield& bf);
-    operator Bitefield() const;
-
-    int GetMaxPower() const;
-    int IsMember(int Elem) const;
-    void InsElem(int Elem);
-    void DelElem(int Elem);
-
-    Set& operator=(const Set& s);
-    bool operator==(const Set& s) const;
-    bool operator!=(const Set& s) const;
-
-    Set operator+(const Set& s) const;
-    Set operator+(int Elem) const;
-    Set operator-(int Elem) const;
-    Set operator*(const Set& s) const;
-    Set operator~() const;
-
-    friend std::istream& operator>>(std::istream& istr, Set& s);
-    friend std::ostream& operator<<(std::ostream& ostr, const Set& s);
-
 private:
     Bitefield BitField;
     int MaxPower;
+
+public:
+    Set(const int mp);
+    Set(const Set& s);
+    Set(const Bitefield& bf);
+    operator Bitefield();
+
+    int GetMaxPower() const;
+    int IsMember(const int Elem) const;
+    void InsElem(const int Elem);
+    void DelElem(const int Elem);
+
+    Set& operator=(const Set& s);
+    int operator==(const Set& s) const;
+    int operator!=(const Set& s) const;
+
+    Set operator+(const Set& s);
+    Set operator+(const int Elem);
+    Set operator-(const int Elem);
+    Set operator*(const Set& s);
+    Set operator~(void);
+
+    friend std::istream& operator>>(std::istream& istr, Set& s);
+    friend std::ostream& operator<<(std::ostream& ostr, const Set& s);
 };
